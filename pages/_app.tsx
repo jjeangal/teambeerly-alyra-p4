afaa80c6fe0f30f4ae3a9462fd63201c1cb996ee
Antoinesrvt <antoine.srvt@gmail.com>
2022-08-01 18:04:08 +0200

    add doss context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,18 +5,17 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "swiper/css/bundle";
 import "../styles/globals.scss";
 import React from "react";
-// import web3Handler from "../services/web3Handler";
 
 const desiredChainId = ChainId.Rinkeby;
-const sdk = new ThirdwebSDK("Rinkeby");
+// const sdk = new ThirdwebSDK("Rinkeby");
 
 export default function OpenBatch({ Component, pageProps }: AppProps) {
-  const getContract = async () => {
-    const NFT = sdk.getContract("0x5fbdb2315678afecb367f032d93f642f64180aa3");
-    const Marketplace = sdk.getContract(
-      "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
-    );
-  };
+  // const getContract = async () => {
+  //   const NFT = sdk.getContract("0x5fbdb2315678afecb367f032d93f642f64180aa3");
+  //   const Marketplace = sdk.getContract(
+  //     "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
+  //   );
+  // };
 
   return (
     <ThirdwebProvider desiredChainId={desiredChainId}>
