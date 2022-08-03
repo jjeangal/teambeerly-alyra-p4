1f9d4c2d6ade9f6634efecf8d65575df42e81325
Nicolas Vairaa <vaira.nicolas@gmail.com>
2022-08-04 00:13:47 +0200

    Add token display page

--- pages/token.tsx
+++
@@ -1,7 +0,0 @@
-import Layout from "../components/Layout/Layout";
-
-// TODO: Use https://nextjs.org/docs/routing/dynamic-routes
-
-export default function Token() {
-  return <Layout>Token Page</Layout>;
-}
