7956559b0429466aa93c20e3bdc27fe2d5283c45
Jean <jgatbkk@gmail.com>
2022-08-06 00:00:54 +0200

    Upload image file to ipfs and get url. Started to implement the add Folder to IPFS.

--- pages/create-collection.tsx
+++ pages/create-collection.tsx
@@ -18,27 +18,21 @@ import {
   Text,
   useNumberInput,
 } from "@chakra-ui/react";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout/Layout";
-import { MarketPlaceContext } from "../context/MarketPlaceContext";
+import { create } from "ipfs-http-client";
 
 export default function CreateCollection() {
-  const { marketPlaceContract } = useContext(MarketPlaceContext);
-
-  async function getFees() {
-    try {
-      const marketPlaceFees = await marketPlaceContract.feePercent();
-      console.log("marketPlaceFees (CreateCollection)", marketPlaceFees);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  useEffect(() => {
-    if (marketPlaceContract) {
-      getFees();
-    }
-  }, [marketPlaceContract]);
+  // Contract type management
+  const [contractType, setContractType] = useState("ERC721");
+  const [name, setName] = useState("");
+  const [symbol, setSymbol] = useState("");
+  const [description, setDescription] = useState("");
+  const [imageFile, setImageFile] = useState<File>();
+  const [imageUrl, setImageUrl] = useState("");
+  const [uriFolder, setUriFolder] = useState<FileList>();
+  const [baseUri, setBaseUri] = useState("");
+  const [supply, setSupply] = useState(0);
 
   // Creator earning management
   const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
@@ -54,38 +48,85 @@ export default function CreateCollection() {
   const dec = getDecrementButtonProps();
   const input = getInputProps();
 
-  // Contract type management
-  const [contractType, setContractType] = useState("ERC721");
+  //Create IPFS clients
+  const projectId = "2CvlZnTIlpRtyaWCJEp0aVOPPUg";
+  const projectSecret = "ac0b1ae7fcabb8bb3972d9fd04d92ae5";
+  const auth =
+    "Basic " + Buffer.from(projectId + ":" + projectSecret).toString("base64");
 
-  const [name, setName] = useState("");
-  const [symbol, setSymbol] = useState("");
-  const [description, setDescription] = useState("");
-  const [imageFile, setImageFile] = useState<File>();
-  const [baseUri, setBaseUri] = useState("");
-  const [supply, setSupply] = useState(0);
+  const client = create({
+    host: "ipfs.infura.io",
+    port: 5001,
+    protocol: "https",
+    headers: {
+      authorization: auth,
+    },
+  });
+
+  const uploadFileToIPFS = async (file: any) => {
+    try {
+      const added = await client.add(file);
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+      return url;
+    } catch (error) {
+      console.log("Error when uploading file to IPFS:", error);
+    }
+  };
+
+  const uploadFolderToIPFS = async (file: any) => {
+    try {
+      //const added = await client.addAll(file);
+      //console.log("added: " + added)
+      //const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+      return null;
+    } catch (error) {
+      console.log("Error when uploading file to IPFS:", error);
+    }
+  };
+
+  const getImageIpfsUrl = async (acceptedFile: File) => {
+    try {
+      const url = await uploadFileToIPFS(acceptedFile);
+      console.log(url);
+      if (url) setImageUrl(url);
+    } catch (error) {
+      console.log("ipfs image upload error: ", error);
+    }
+  };
+
+  const getUriIpfs = async (acceptedFile: FileList) => {
+    try {
+      const url = await uploadFolderToIPFS(acceptedFile);
+      console.log(url);
+      if (url) setBaseUri(url);
+    } catch (error) {
+      console.log("ipfs image upload error: ", error);
+    }
+  };
 
   function createCollection() {
     if (name.length == 0) return alert("Name field is empty.");
     if (symbol.length == 0) return alert("Symbol field is empty.");
-    if (description.length == 0) alert("Description field is empty.");
-    if (baseUri.length == 0) alert("Uri field is empty.");
-    if (supply <= 0) alert("Supply must be higher than 0.");
-
+    if (description.length == 0) return alert("Description field is empty.");
+    if (!imageFile) return alert("Choose a collection image.");
+    if (!uriFolder) return alert("Choose images for collection nfts.");
+    if (supply <= 0) return alert("Supply must be higher than 0.");
     generateIpfsLinks();
     generateMetaData();
   }
 
   function generateIpfsLinks() {
     console.log("Generate folder link to IPFS");
-    console.log("Generate image link to IPFS");
+    if (uriFolder) getUriIpfs(uriFolder);
+    console.log("Generate image link to IPFS:");
+    if (imageFile) getImageIpfsUrl(imageFile);
   }
 
   function generateMetaData() {
     const metadata = {
       name: name,
       description: description,
-      image:
-        "https://ipfs.io/ipfs/QmUnMkaEB5FBMDhjPsEtLyHr4ShSAoHUrwqVryCeuMosNr",
+      image: imageUrl,
       external_url: baseUri,
     };
     console.log(metadata);
@@ -156,9 +197,12 @@ export default function CreateCollection() {
               will give "https://ipfs.io/ipfs/QvTalyhCoX/3".
             </FormHelperText>
             <Input
-              type="text"
-              value={baseUri}
-              onChange={(e) => setBaseUri(e.target.value)}
+              type="file"
+              accept="image/png, image/jpeg"
+              multiple={true}
+              onChange={(e) => {
+                if (e.target.files != null) setUriFolder(e.target.files);
+              }}
             />
           </FormControl>
         </Box>
@@ -187,7 +231,6 @@ export default function CreateCollection() {
             </HStack>
           </FormControl>
         </Box>
-
         <Box mt={"2em"} w={"full"}>
           <FormControl>
             <FormLabel>Supply</FormLabel>
--- pages/create-token.tsx
+++ pages/create-token.tsx
@@ -1,15 +1,15 @@
 import Layout from "../components/Layout/Layout";
 import { useState, useContext, useCallback } from "react";
 
-import { NFTContext } from "../context/NFTContext.js";
+//import { NFTContext } from "../context/NFTContext.js";
 
 export default function CreateToken() {
   const [fileUrel, setFileUrl] = useState(null);
-  const { uploadToIPFS } = useContext(NFTContext);
+  // const { uploadToIPFS } = useContext(NFTContext);
 
   const onDrop = useCallback(async (file: any[]) => {
-    const url = await uploadToIPFS(file[0]);
-    setFileUrl(url);
+    // const url = await uploadToIPFS(file[0]);
+    //setFileUrl(url);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
